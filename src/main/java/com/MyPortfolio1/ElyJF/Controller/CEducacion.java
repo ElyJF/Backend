0ed0dc3835fb19aa061ac68bec7aef1139b4a655
ElyJF <ely_mariano_as@hotmail.com>
2023-04-24 12:47:01 -0300

    render

--- src/main/java/com/MyPortfolio1/ElyJF/Controller/CEducacion.java
+++ src/main/java/com/MyPortfolio1/ElyJF/Controller/CEducacion.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://myportfolio-73c6f.web.app"})
 public class CEducacion {
 
     @Autowired
--- src/main/java/com/MyPortfolio1/ElyJF/Controller/CExperiencia.java
+++ src/main/java/com/MyPortfolio1/ElyJF/Controller/CExperiencia.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://myportfolio-73c6f.web.app"})
 public class CExperiencia {
 
     @Autowired
